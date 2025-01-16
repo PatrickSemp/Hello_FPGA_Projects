196db423b9b0fdf8905fb9a8160d0e0716a3418f
PatrickSemp <basspedrosanchez@gmail.com>
2025-01-15 23:29:09 -0500

    Add files via upload

---
+++ SOFTCONSOLE/Prueba43_Multiplicacion_Matrices_2/MSS_System_hw_platform.h
@@ -0,0 +1,17 @@
+#ifndef MSS_System_HW_PLATFORM_H_
+#define MSS_System_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Mon May  6 16:52:36 2024
+*
+*Memory map specification for peripherals in MSS_System
+*/
+
+/*-----------------------------------------------------------------------------
+* CM3 subsystem memory map
+* Initiator(s) for this subsystem: CM3 
+*---------------------------------------------------------------------------*/
+#define COREUARTAPB_0_0                 0x50000000U
+
+
+#endif /* MSS_System_HW_PLATFORM_H_*/
---
+++ SOFTCONSOLE/Prueba43_Multiplicacion_Matrices_2/README.txt
@@ -0,0 +1,6 @@
+Export Firmware README
+
+Microchip Technology Inc. - Microchip Libero Software Release v2023.2 (Version 2023.2.0.8)
+
+Date    :    Mon May  6 16:52:36 2024
+Project :    D:\ESPE\TESIS\SW_LIBERO\PROGRAMAS\B_PRUEBAS\Prueba43_Multiplicacion_Matrices_User
