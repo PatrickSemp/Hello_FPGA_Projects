706f894ebaceba9d5eb30fae2e56a7bfc9e0a032
PatrickSemp <basspedrosanchez@gmail.com>
2025-01-15 22:27:52 -0500

    Add files via upload

---
+++ SOFTCONSOLE/01_MDDR_RAM_TMR/Debug/main.d
@@ -0,0 +1,25 @@
+main.o: ../main.c ../drivers/mss_sys_services/mss_sys_services.h \
+ ../drivers/mss_sys_services/../../CMSIS/m2sxxx.h \
+ C:/Microsemi/SoftConsole_v4.0/CMSIS/V4.3/Include/core_cm3.h \
+ C:/Microsemi/SoftConsole_v4.0/CMSIS/V4.3/Include/core_cmInstr.h \
+ C:/Microsemi/SoftConsole_v4.0/CMSIS/V4.3/Include/core_cmFunc.h \
+ ../drivers/mss_sys_services/mss_comblk_page_handler.h ../hal/hal.h \
+ ../hal/CortexM3/GNU/cpu_types.h ../hal/hw_reg_access.h
+
+../drivers/mss_sys_services/mss_sys_services.h:
+
+../drivers/mss_sys_services/../../CMSIS/m2sxxx.h:
+
+C:/Microsemi/SoftConsole_v4.0/CMSIS/V4.3/Include/core_cm3.h:
+
+C:/Microsemi/SoftConsole_v4.0/CMSIS/V4.3/Include/core_cmInstr.h:
+
+C:/Microsemi/SoftConsole_v4.0/CMSIS/V4.3/Include/core_cmFunc.h:
+
+../drivers/mss_sys_services/mss_comblk_page_handler.h:
+
+../hal/hal.h:
+
+../hal/CortexM3/GNU/cpu_types.h:
+
+../hal/hw_reg_access.h:
